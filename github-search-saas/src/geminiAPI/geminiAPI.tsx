184118bcedc439ec1a414a2f1602d396db338666
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-04-12 17:09:24 +0800

    migrate to genai sdk

--- github-search-saas/src/geminiAPI/geminiAPI.tsx
+++ github-search-saas/src/geminiAPI/geminiAPI.tsx
@@ -1,62 +1,125 @@
-import { GenerationConfig, GoogleGenerativeAI } from "@google/generative-ai";
 import { GEMINI_API_KEY } from "../api/apiconfigs";
+import { GoogleGenAI, GenerateContentConfig, Type } from "@google/genai";
 
 const apiKey = GEMINI_API_KEY;
-const genAI = new GoogleGenerativeAI(apiKey);
+const genAI = new GoogleGenAI({ apiKey: apiKey });
 
-const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
-});
+const defaultModel = "gemini-2.0-flash";
 
-const generationConfig = {
+const defaultGenerationConfig = {
   temperature: 1,
   topP: 0.95,
   topK: 40,
   maxOutputTokens: 8192,
-  responseModalities: [],
   responseMimeType: "text/plain",
 };
 
 export async function generateContent(prompt: string) {
-  const chatSession = model.startChat({
-    generationConfig,
+  const result = await genAI.models.generateContent({
+    model: defaultModel,
+    config: defaultGenerationConfig,
+    contents: prompt,
   });
 
-  const result = await chatSession.sendMessage(prompt);
-
-  return result.response.text();
+  return result.text;
 }
 
-export async function generateContentWithConfig(prompt: string, config: GenerationConfig) {
-  if (config == null) {
-    throw new Error("Config is null. Please provide a valid config to api.");
-  }
-
-  const chatSession = model.startChat({
-    generationConfig: config,
+export async function generateContentWithConfig(
+  prompt: string,
+  config: GenerateContentConfig
+) {
+  const result = await genAI.models.generateContent({
+    model: defaultModel,
+    config: config,
+    contents: prompt,
   });
 
-  const result = await chatSession.sendMessage(prompt);
-  
-  return result.response.text();
+  return result.text;
 }
 
-export async function generateWithSystemInstructionAndConfig(systemInstruction: string, prompt: string, config: GenerationConfig) {
-  if (config == null) {
-    throw new Error("Config is null. Please provide a valid config to api.");
-  }
+export async function generateWithSystemInstructionAndConfig(
+  systemInstruction: string,
+  prompt: string,
+  config: GenerateContentConfig
+) {
+  const result = await genAI.models.generateContent({
+    model: defaultModel,
+    config: {
+      ...config,
+      systemInstruction: {
+        role: "user",
+        parts: [{ text: systemInstruction }],
+      },
+    },
+    contents: prompt,
+  });
 
-  const chatSession = model.startChat({
-    generationConfig: config,
-    systemInstruction: {
-      role: "user",
-      parts: [
-        {text: systemInstruction},
+  return result.text;
+}
+
+export async function generateWithSystemInstructionConfigAndTools(
+  systemInstruction: string,
+  prompt: string,
+  config: GenerateContentConfig
+) {
+  const result = await genAI.models.generateContent({
+    model: defaultModel,
+    config: {
+      ...config,
+      systemInstruction: {
+        role: "user",
+        parts: [{ text: systemInstruction }],
+      },
+      tools: [
+        {
+          functionDeclarations: [submitPullRequestFunctionDeclaration],
+        },
       ],
     },
+    contents: prompt,
   });
 
-  const result = await chatSession.sendMessage(prompt);
-  
-  return result.response.text();
+  return result.text;
 }
+
+const submitPullRequestFunctionDeclaration = {
+  name: "submit_pull_request",
+  description: "Submit a pull request to the repository.",
+  parameters: {
+    type: Type.OBJECT,
+    properties: {
+      filePath: {
+        type: Type.STRING,
+        description: 'path of the file with file name, e.g. "README.md"',
+      },
+      commitMessage: {
+        type: Type.STRING,
+        description: "commit message of the change",
+      },
+      branchName: {
+        type: Type.STRING,
+        description: "branch name created for the pull request",
+      },
+      pullRequestTitle: {
+        type: Type.STRING,
+        description: "The title of the pull request.",
+      },
+      pullRequestBody: {
+        type: Type.STRING,
+        description: "The body of the pull request.",
+      },
+      fileContent: {
+        type: Type.STRING,
+        description: "The content of the file.",
+      },
+    },
+    required: [
+      "filePath",
+      "commitMessage",
+      "branchName",
+      "pullRequestTitle",
+      "pullRequestBody",
+      "fileContent",
+    ],
+  },
+};
