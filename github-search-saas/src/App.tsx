b18f250e70dc5cdaf247741f75378ff8534cc4c0
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-04-12 11:36:05 +0800

    highlight tab when selected

--- github-search-saas/src/App.tsx
+++ github-search-saas/src/App.tsx
@@ -12,9 +12,24 @@ function App() {
       {/* side bar */}
       <div className="flex flex-col gap-4 items-center bg-gray-800 p-4 max-w-1/6 h-full">
         <h2 className="text-lg text-white">Tabs</h2>
-        <p className="text-lg text-gray-400 hover:text-white" onClick={()=>setTab('GeneralInfo')}>GeneralInfo</p>
-        <p className="text-lg text-gray-400 hover:text-white" onClick={()=>setTab('CodeSearch')}>CodeSearch</p>
-        <p className="text-lg text-gray-400 hover:text-white" onClick={()=>setTab('CodeEdit')}>CodeEdit</p>
+        <p
+          className={`text-lg text-gray-400 hover:text-white ${tab === 'GeneralInfo' ? 'text-white' : ''}`}
+          onClick={() => setTab('GeneralInfo')}
+        >
+          GeneralInfo
+        </p>
+        <p
+          className={`text-lg text-gray-400 hover:text-white ${tab === 'CodeSearch' ? 'text-white' : ''}`}
+          onClick={() => setTab('CodeSearch')}
+        >
+          CodeSearch
+        </p>
+        <p
+          className={`text-lg text-gray-400 hover:text-white ${tab === 'CodeEdit' ? 'text-white' : ''}`}
+          onClick={() => setTab('CodeEdit')}
+        >
+          CodeEdit
+        </p>
       </div>
       {/* main content */}
       <GithubProvider>
