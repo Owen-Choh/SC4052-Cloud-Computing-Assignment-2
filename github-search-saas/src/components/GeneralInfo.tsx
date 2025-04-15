da2946d478ded8b9a632c1ca7837f405562b122a
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-04-15 14:53:20 +0800

    style general info ui

--- github-search-saas/src/components/GeneralInfo.tsx
+++ github-search-saas/src/components/GeneralInfo.tsx
@@ -41,7 +41,9 @@ const GeneralInfo: React.FC = () => {
           setRepos(repoNames);
         }
       } catch (error) {
-        setError("An error occurred while fetching repositories. Are you sure the username and token is valid?");
+        setError(
+          "An error occurred while fetching repositories. Are you sure the username and token is valid?"
+        );
       } finally {
         setLoading(false);
       }
@@ -56,68 +58,90 @@ const GeneralInfo: React.FC = () => {
         <h2 className="text-2xl">General Information</h2>
       </div>
       <div className="flex flex-col gap-4 mt-4">
-        <label className="flex gap-2 text-lg items-center">
-          Gemini API Key:
-          <input
-            type="text"
-            value={geminiApiKey}
-            onChange={(e) => setGeminiApiKey(e.target.value)}
-          />
-        </label>
-        <label className="flex gap-2 text-lg items-center">
-          GitHub Token:
-          <input
-            type="password"
-            value={token}
-            onChange={(e) => {
-              setToken(e.target.value);
-              setOctokit(e.target.value);
-            }}
-          />
-        </label>
-        <label className="flex gap-2 text-lg items-center">
-          GitHub Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label className="flex gap-2 text-lg items-center">
-          Repository Name:
-          <button
-            onClick={fetchRepos}
-            disabled={loading}
-            className="bg-blue-500 text-white px-4 py-2 rounded"
-          >
-            {loading ? "Fetching..." : "Get Repos"}
-          </button>
+        <div className="flex flex-col gap-2 border-2 border-gray-700 rounded-lg p-4">
+          <label className="flex gap-2 text-lg items-center">
+            Gemini API Key:
+            <input
+              type="text"
+              value={geminiApiKey}
+              onChange={(e) => setGeminiApiKey(e.target.value)}
+            />
+          </label>
+          <p>
+            This is used to generate code descriptions and summaries. You can
+            get a free API key from google gemini.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 border-2 border-gray-700 rounded-lg p-4">
+          <label className="flex gap-2 text-lg items-center">
+            GitHub Token:
+            <input
+              type="text"
+              value={token}
+              onChange={(e) => {
+                setToken(e.target.value);
+                setOctokit(e.target.value);
+              }}
+            />
+          </label>
+          <p>
+            This is used to list the repos that you own, search and retrieve
+            code in the repository. Optionally, also submit pull requests if you
+            use one of the features provided.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 border-2 border-gray-700 rounded-lg p-4">
+          <label className="flex gap-2 text-lg items-center">
+            GitHub Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <p>
+            Enter your GitHub username to fetch repositories and interact with
+            the GitHub API.
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 border-2 border-gray-700 rounded-lg p-4">
+          <label className="flex gap-2 text-lg items-center">
+            Repository Name:
+            <button
+              onClick={fetchRepos}
+              disabled={loading}
+              className="bg-blue-500 text-white !p-1 rounded"
+            >
+              {loading ? "Fetching..." : "List Repos"}
+            </button>
+          </label>
+          <p>
+            Select a repository from the list of repositories fetched using your
+            GitHub username.
+          </p>
           {loading ? (
             <span>Loading...</span>
           ) : (
-            <>
-              <select
-                value={repository}
-                onChange={(e) => {
-                  const selectedRepo = e.target.value;
-                  setRepository(selectedRepo); // Update context provider
-                }}
-              >
-                <option value="" className="text-black">
-                  Select a repository
+            <select
+              value={repository}
+              onChange={(e) => {
+                const selectedRepo = e.target.value;
+                setRepository(selectedRepo); // Update context provider
+              }}
+            >
+              <option value="" className="text-black">
+                Select a repository
+              </option>
+              {repos.map((repo) => (
+                <option key={repo} value={repo} className="text-black">
+                  {repo}
                 </option>
-                {repos.map((repo) => (
-                  <option key={repo} value={repo} className="text-black">
-                    {repo}
-                  </option>
-                ))}
-              </select>
-            </>
+              ))}
+            </select>
           )}
-          
-        </label>
-        {error && <span className="text-lg text-red-500">{error}</span>}
+          {error && <span className="text-lg text-red-500">{error}</span>}
         </div>
+      </div>
     </div>
   );
 };
