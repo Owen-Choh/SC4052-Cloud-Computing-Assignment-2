3cce369c4f60224808d32d9a6c0136ec1856e539
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-05-30 11:10:33 +0800

    add base url to vite config

--- github-search-saas/vite.config.ts
+++ github-search-saas/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/SC4052-Cloud-Computing-Assignment-2/',
   plugins: [
     react(),
     tailwindcss(),
